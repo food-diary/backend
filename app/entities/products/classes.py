332b25e45e5cd65850324c5d5e80e55798c6ee68
Евгений <odosaol@yandex.ru>
2024-11-29 18:22:21 +0500

    description product class

--- app/entities/products/classes.py
+++ app/entities/products/classes.py
@@ -1,14 +1,15 @@
-from pydantic import BaseModel
+from typing import Optional
 
+from pydantic import BaseModel, Field, NonNegativeFloat, NonNegativeInt
 
 
 class ProductBase(BaseModel):
-    name: str
-    description: str
-    proteins: float
-    fats: float
-    carbohydrates: float
-    calories: float
+    name: str = Field(..., min_length=2, max_digits=15, description="Название продукта")
+    description: Optional[str] = Field(None, max_length=100)
+    proteins: NonNegativeFloat = Field(0.0, ge=0,description="Кол-во белков(на 100г.)")
+    fats: NonNegativeFloat = Field(0.0, ge=0,description="Кол-во жиров(на 100г.)")
+    carbohydrates: NonNegativeFloat = Field(0.0, ge=0,description="Кол-во углеводов(на 100г.)")
+    calories: NonNegativeFloat = Field(0.0, ge=0,description="Кол-во калорий(на 100г.)")
     
     
 class ProductCreate(ProductBase):
@@ -16,7 +17,7 @@ class ProductCreate(ProductBase):
 
 
 class Product(ProductBase):
-    id: int
+    id: NonNegativeInt = Field(..., ge=1, description="ID продукта(автогенерация в БД-)")
     
     class Config:
         from_attributes = True
