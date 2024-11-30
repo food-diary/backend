49a6023d166ded7164cc49bfd00ec08fdba2541f
Евгений <odosaol@yandex.ru>
2024-11-30 18:01:32 +0500

    change uvicorn conn

--- main.py
+++ main.py
@@ -22,4 +22,4 @@ if __name__ == "__main__":
     
     asyncio.run(init_app())
     
-    uvicorn.run(app=app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
