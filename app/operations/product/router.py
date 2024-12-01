298a3c8f71f9178b9f090fc180e19e8dcafde198
Евгений <odosaol@yandex.ru>
2024-12-01 14:43:08 +0500

    add func

--- app/operations/product/router.py
+++ app/operations/product/router.py
@@ -8,7 +8,8 @@ from app.database.connect import get_session, AsyncSession
 from app.entities.products.classes import Product
 from app.entities.products.models import Product as ProductDB
 
-from app.operations.product.crud import get_all_product as get
+from app.operations.product.crud import get_all_product as get_all
+from app.operations.product.crud import get_product_by_id as get_by_id
 
 
 router = APIRouter(prefix="/product",
@@ -17,4 +18,11 @@ router = APIRouter(prefix="/product",
 
 @router.get('/all', response_model=List[Product])
 async def get_all_product(session: AsyncSession = Depends(get_session)) -> List[Product]:
-    return await get(session)
+    return await get_all(session)
+
+
+@router.get("/{id}", response_model=Product)
+async def get_product_by_id(id: int,
+                            session: AsyncSession = Depends(get_session)) -> Product:
+    return await get_by_id(id,
+                     session=session)
