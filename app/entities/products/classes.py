89e97d8d7664228fba3347e375657340dd1afecf
Евгений <odosaol@yandex.ru>
2024-12-01 14:35:54 +0500

    fix pydantic model

--- app/entities/products/classes.py
+++ app/entities/products/classes.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field, NonNegativeFloat, NonNegativeInt
 
 
 class ProductBase(BaseModel):
-    name: str = Field(..., min_length=2, max_digits=15, description="Название продукта")
+    name: str = Field(..., min_length=2, max_length=35, description="Название продукта")
     description: Optional[str] = Field(None, max_length=100)
     proteins: NonNegativeFloat = Field(0.0, ge=0,description="Кол-во белков(на 100г.)")
     fats: NonNegativeFloat = Field(0.0, ge=0,description="Кол-во жиров(на 100г.)")
--- main.py
+++ main.py
@@ -12,12 +12,14 @@ from app.operations.product.router import router as product_router
 app = FastAPI(title="Diary")
 app.include_router(product_router)
 
-@app.get('/')
+
+@app.get("/")
 async def hello() -> str:
     return "Hello"
 
+
 if __name__ == "__main__":
-    
+
     asyncio.run(create_database())
-    
+
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
