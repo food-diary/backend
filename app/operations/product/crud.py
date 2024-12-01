03ed2c557bee5a8b7941233bf2d2851656e43049
Евгений <odosaol@yandex.ru>
2024-12-01 15:54:12 +0500

    create update product func

--- app/operations/product/crud.py
+++ app/operations/product/crud.py
@@ -5,7 +5,7 @@ from sqlalchemy import select
 
 from app.database.connect import AsyncSession
 
-from app.entities.products.classes import Product
+from app.entities.products.classes import Product, ProductCreate
 from app.entities.products.models import Product as ProductDB
 
 
@@ -41,3 +41,26 @@ async def delete_product_by_id(id: int, session: AsyncSession) -> Response:
     return Response(
         status_code=204, headers={"message": "The product has been deleted!"}
     )
+
+
+async def update_product_by_id(id: int,
+                               product: ProductCreate,
+                               session: AsyncSession) -> Product:
+    query = await session.execute(select(ProductDB).where(ProductDB.id == id))
+    result = query.scalar_one_or_none()
+    
+    if result is None:
+        raise HTTPException(status_code=401, detail="Not found!")
+    
+    result.name = product.name
+    result.description = product.description
+    result.proteins = product.proteins
+    result.fats = product.fats
+    result.carbohydrates = product.carbohydrates
+    result.calories = product.calories
+
+    
+    await session.commit()
+    await session.refresh(result)
+    
+    return result
