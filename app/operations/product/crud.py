985e37615f444213ca1597cdd3d3bcc3c5bb28a1
Евгений <odosaol@yandex.ru>
2024-12-01 13:08:23 +0500

    fix

---
+++ app/operations/product/crud.py
@@ -0,0 +1,20 @@
+from typing import List
+
+from fastapi import HTTPException
+from sqlalchemy import select
+
+from app.database.connect import get_session, AsyncSession
+
+from app.entities.products.classes import Product
+from app.entities.products.models import Product as ProductDB
+
+
+async def get_all_product(session: AsyncSession) -> List[Product]:
+    query = await session.execute(select(ProductDB))
+    result = query.scalars().all()
+    
+    if not result:
+        raise HTTPException(status_code=401,
+                            detail="Not found!")
+    
+    return result
