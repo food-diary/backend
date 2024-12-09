0c12530698f3362fd49c6d2470c3a4283e246ec5
Евгений <odosaol@yandex.ru>
2024-12-09 17:27:21 +0500

    add classes

---
+++ app/entities/diary/classes.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+import datetime
+
+from pydantic import BaseModel, Field, NonNegativeFloat, NonNegativeInt, date
+
+
+class DiaryBase(BaseModel):
+    product_id: int = Field(..., description="ID продукта из базы данных")
+    date: datetime.date = Field(..., description="Дата добавления продукта")
+    
+class Diary(DiaryBase):
+    id: NonNegativeInt = Field(..., ge=1, description="ID записи(автогенерация в БД)")
+    
+class DiaryCreate(BaseModel):
+    user_id: int = Field(..., ge=1,description="ID пользователя,который добавляет продукт")
