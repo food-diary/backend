438fd0c215fa0a6270a8872f0001920c35522c19
Евгений <odosaol@yandex.ru>
2024-12-13 01:52:37 +0500

    changed the return of a string instead of bytes

--- app/entities/diary/classes.py
+++ app/entities/diary/classes.py
@@ -5,15 +5,8 @@ from pydantic import BaseModel, Field, NonNegativeFloat, NonNegativeInt
 
 class DiaryBase(BaseModel):
     product_id: int = Field(..., description="ID продукта из базы данных")
-<<<<<<< HEAD
-<<<<<<< HEAD
     count: int = Field(0,ge=1, description="Кол-во продукта в граммах")
-=======
-    count: NonNegativeFloat = Field(..., description="Кол-во продукта в граммах")
->>>>>>> 1389e72 (add get_for_day func and add_new_record func)
-=======
     count: NonNegativeInt = Field(..., description="Кол-во продукта в граммах")
->>>>>>> 696eb06 (some edit database)
     day: date = Field(default_factory=date.today, description="Дата добавления продукта")
     
 class Diary(DiaryBase):
--- app/entities/diary/models.py
+++ app/entities/diary/models.py
@@ -8,19 +8,10 @@ class Diary(Base):
     __tablename__ = "users_diary"
     
     id = Column(Integer, primary_key=True, index=True)
-<<<<<<< HEAD
-    user_id = Column(Integer, ForeignKey("users.id"), index=True, )
-    product_id = Column(Integer, ForeignKey("products.id"))
-<<<<<<< HEAD
     count = Column(Integer)
-=======
-    count = Column(Float)
->>>>>>> 1389e72 (add get_for_day func and add_new_record func)
-=======
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), index=True)
     product_id = Column(Integer, ForeignKey("products.id", ondelete="CASCADE"))
     count = Column(Integer)
->>>>>>> 696eb06 (some edit database)
     day = Column(Date)
     
     diary_user = relationship("User", back_populates="user_diary")
