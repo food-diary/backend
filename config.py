81ceb0a8e43c9f728d5d4bb5dc9d46254c2fd931
Евгений <odosaol@yandex.ru>
2024-12-13 01:29:53 +0500

    Успешная миграция и переход на postgresql

--- config.py
+++ config.py
@@ -4,9 +4,19 @@ import os
 
 load_dotenv()
 
-SQL_DB = os.getenv("SQL_DB")
-SQL_SYNC_DB = SQL_DB.replace("sqlite+aiosqlite", "sqlite")
+# SQL_DB = os.getenv("SQL_DB")
+# SQL_SYNC_DB = SQL_DB.replace("sqlite+aiosqlite", "sqlite")
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 ALGORITHM = os.getenv('ALGORITHM')
 EXPIRE = int(os.getenv('EXPIRE'))
+
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_USER = os.getenv('DB_USER')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+
+
+SQL_DB = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+SQL_SYNC_DB = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
